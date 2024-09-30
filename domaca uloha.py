4d877e52bd2fa5716c0c0061f34e51ed13b91eba
jakubkendera <180315366+jakubkendera@users.noreply.github.com>
2024-09-30 17:04:19 +0200

    Add files via upload

---
+++ domaca uloha.py
@@ -0,0 +1,100 @@
+#1.
+def cisla():
+    for i in range (1,11):
+        print (i)
+
+
+#2.
+N=float(input("zadaj cislo"))
+def numbers():
+    for j in range (1,N+1):
+        return j
+print (numbers)
+
+
+#3.
+def kazde_druhe_cislo():
+    for i in range(5, N + 1, 2):
+        print(i)
+
+
+
+
+#4.
+def mocniny():
+    for i in range((1 ** 2), ((N ** 2) + 1)):
+        return i
+print(mocniny)
+
+
+
+#5.
+odkial=float(input("napis odkial"))
+pokial=float(input("napis pokial"))
+def cisla_a_mocniny(odkial, pokial):
+    for i in range (odkial**0.5,pokial**0.5):
+        print (cisla_a_mocniny())
+
+
+#6
+def funkcne_hodnoty():
+    for x in range (1,20):
+        y=((x**2)-1)/(x-3)
+
+    print(funkcne_hodnoty())
+
+#7
+def delenie_cisel():
+    for n in range  (1,N):
+        if (n%3)==0 :
+            print (n)
+
+
+#8
+#if o in range ():
+        #(o%3) = 0:
+         #print (o)
+
+
+#9
+#z=float(input("zadaj zacaitocne cislo"))
+#k=float(input("zadaj konecne cislo"))
+#def neparne_cisla():
+        #    for p in range (z,k):
+            #return i
+#if p%2<0:
+#   print (p)
+#if p%2>0:
+#    print (p)
+
+#10
+def cisla_odzadu():
+    for r in range (N,N-1,N-N):
+        print (r)
+
+#11
+def cisla_delitelne_4_7():
+    for s in range (0,N):
+        return s
+#if s%7==0:
+#   print (s)
+#elif s%4==0:
+    print (s)
+
+
+#12
+#def dokopy():
+    #   for t in range(1,N):
+    #    scitanie=(1+t):
+#            print (scitanie)
+
+#13
+def scitanie():
+#    for i in range(1,N):
+#        return i
+#if i%2==0:
+#    print (i+i)
+
+#14
+
+
